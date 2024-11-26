1ba05877c09081aa68c38257118f409df78c4079
CHANDANKUMARNS07 <Chandankumar.ns@iiitb.ac.in>
2024-11-26 05:58:20 +0530

    Add files via upload

---
+++ VSDbabySOC_chandan/vsdbabysoc.v
@@ -0,0 +1,40 @@
+
+module vsdbabysoc (
+   output wire OUT,
+   //
+   input  wire reset,
+   //
+   input  wire VCO_IN,
+   input  wire ENb_CP,
+   input  wire ENb_VCO,
+   input  wire REF,
+   //
+   // input  wire VREFL,
+   input  wire VREFH
+);
+
+   wire clk_chandan;
+   wire [9:0] RV_TO_DAC;
+
+   riscv_chandan core (
+      .OUT(RV_TO_DAC),
+      .CLK(clk_chandan),
+      .reset(reset)
+   );
+
+   avsdpll avsdpll (
+      .CLK(clk_chandan),
+      .VCO_IN(VCO_IN),
+      .ENb_CP(ENb_CP),
+      .ENb_VCO(ENb_VCO),
+      .REF(REF)
+   );
+
+   avsddac avsddac (
+      .OUT(OUT),
+      .D(RV_TO_DAC),
+      // .VREFL(VREFL),
+      .VREFH(VREFH)
+   );
+   
+endmodule
